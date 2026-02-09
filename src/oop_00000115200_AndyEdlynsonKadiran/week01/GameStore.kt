9c61407824ff0a9e605a74b67039193a172a6565
Your Name <andy.ek788@gmail.com>
2026-02-09 15:13:49 +0700

    week02: create basic Student class structure

--- src/oop_00000115200_AndyEdlynsonKadiran/week01/GameStore.kt
+++ src/oop_00000115200_AndyEdlynsonKadiran/week01/GameStore.kt
@@ -3,18 +3,19 @@ package oop_00000115200_AndyEdlynsonKadiran.week01
 fun main(args: Array<String>) {
     val gameTitle: String = "Call of Duty"
     val price: Int = 400000
+    val userNote: String? = null
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
 
     println($"Calculated Discounted Price of Game is $${calculateDiscount(price)}")
 
-    printReceipt(title = gameTitle, finalPrice = finalPrice)
+    printReceipt(title = gameTitle, finalPrice = finalPrice, note = userNote)
 }
 
 fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
-fun printReceipt(title: String, finalPrice: Int) {
+fun printReceipt(title: String, finalPrice: Int, note: String?) {
     println("===== RECEIPT =====")
     println("Game Title : $title")
     println($$"Final Price: $$$finalPrice")
---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week02/Student.kt
@@ -0,0 +1,9 @@
+package oop_00000115200_AndyEdlynsonKadiran.week02
+
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String
+){
+
+}
