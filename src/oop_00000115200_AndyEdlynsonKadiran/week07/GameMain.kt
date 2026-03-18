99b7497fac2f06a63378540151b57ced3b012b43
Your Name <andy.ek788@gmail.com>
2026-03-18 23:39:28 +0700

    week07: (task) test GameManager singleton in main

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week07/GameMain.kt
@@ -0,0 +1,9 @@
+package oop_00000115200_AndyEdlynsonKadiran.week07
+
+fun main() {
+
+    println("\n=== TEST GAME MANAGER SINGLETON ===")
+    GameManager.startGame()
+
+    GameManager.startGame()
+}
