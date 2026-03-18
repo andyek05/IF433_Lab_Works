ecedf6215ccd6279bebdad8795c5a94ba445b8b5
Your Name <andy.ek788@gmail.com>
2026-03-18 23:32:07 +0700

    week07: (task) creae ItenRarity enum

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000115200_AndyEdlynsonKadiran.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(30),
+    RARE(10),
+    EPIC(5),
+    LEGENDARY(1)
+}
