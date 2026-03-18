de193b07a19169dde9fc0bfa4a7a6e84c2a2aea5
Your Name <andy.ek788@gmail.com>
2026-03-18 22:51:48 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week06/TugasMain.kt
@@ -0,0 +1,7 @@
+package oop_00000115200_AndyEdlynsonKadiran.week06
+
+fun main() {
+    val lampuTamu = SmartLamp(id = "L01", name = "Ruang Tamu")
+    val speakerDapur = SmartSpeaker(id = "S01", name = "Google Nest Dapur")
+    val cctvGarasi = SmartCCTV(id = "C01", name = "Ezviz Garasi")
+}
