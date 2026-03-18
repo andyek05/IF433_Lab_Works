942386655ab9315b4f72e01369fdaf6ac28105c3
Your Name <andy.ek788@gmail.com>
2026-03-18 22:16:07 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000115200_AndyEdlynsonKadiran.week06
+
+interface Camera {
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
