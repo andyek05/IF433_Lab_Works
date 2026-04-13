0ad7d029ac9c727c7a24f308abdeac9eb45c8bca
Your Name <andy.ek788@gmail.com>
2026-04-13 15:24:06 +0700

    week08: imeplement smart cast in function

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week08/NotificationService.kt
@@ -0,0 +1,15 @@
+package oop_00000115200_AndyEdlynsonKadiran.week08
+
+class NotificationService{
+    fun sendEmail(emailAddress: String){
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile){
+        if(user.email != null){
+            sendEmail(user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
