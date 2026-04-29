87bea6779c254c8c29425c9911991079771c7c95
Your Name <andy.ek788@gmail.com>
2026-04-29 22:37:09 +0700

    week09: (task) create TradeLog data classes

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week09/TradingModels.kt
@@ -0,0 +1,11 @@
+package oop_00000115200_AndyEdlynsonKadiran.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
+
+
