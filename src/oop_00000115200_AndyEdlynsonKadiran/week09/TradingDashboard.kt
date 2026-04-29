6c0e35cc36b7421a34ee27b821099cf5279cc334
Your Name <andy.ek788@gmail.com>
2026-04-29 23:08:43 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week09/TradingDashboard.kt
@@ -0,0 +1,18 @@
+package oop_00000115200_AndyEdlynsonKadiran.week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),   // Win
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),  // Loss
+        TradeLog("BTCUSDT", "SHORT", 50, 120.0, "CLOSED"), // Win
+        TradeLog("SOLUSDT", "LONG", 20, -10.0, "CLOSED"),  // Loss
+        TradeLog("ETHUSDT", "LONG", 10, 2.5, "OPEN"),      // Masih jalan (Profit)
+        TradeLog("BTCUSDT", "SHORT", 10, -1.0, "OPEN"),    // Masih jalan (Loss)
+        TradeLog("SOLUSDT", "SHORT", 25, 45.0, "CLOSED")   // Win
+    )
+
+    println("--- Data Log Perdagangan Berhasil Dimuat ---")
+    tradeHistory.forEach { println(it) }
+
+}
