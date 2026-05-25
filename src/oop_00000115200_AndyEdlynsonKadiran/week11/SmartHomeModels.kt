d1bd22848b1c9f5c785c514b6157aab26f4a6461
Your Name <andy.ek788@gmail.com>
2026-05-25 12:28:48 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_00000115200_AndyEdlynsonKadiran/week11/SmartHomeModels.kt
+++ src/oop_00000115200_AndyEdlynsonKadiran/week11/SmartHomeModels.kt
@@ -2,3 +2,6 @@ package oop_00000115200_AndyEdlynsonKadiran.week11
 
 data class SmartDevice(var name: String, val category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
 
+fun SmartDevice.diagnose(): String{
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
+}
