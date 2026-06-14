8cfb7baf8c4889ecaebcc11b0d7ca45a54b36c5a
Your Name <andy.ek788@gmail.com>
2026-06-14 21:31:52 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000115200_AndyEdlynsonKadiran/week12/SmartFeeder.kt
+++ src/oop_00000115200_AndyEdlynsonKadiran/week12/SmartFeeder.kt
@@ -19,4 +19,23 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 fun main() {
     var currentKibbleStock = 50
 
+    println("--- Simulasi Jadwal Makan Pagi ---")
+
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+        println("Sisa stok kibble sekarang: $currentKibbleStock gr")
+
+    } catch (e: DispenserJamException) {
+        println("[ERROR] Dispenser Macet: ${e.message}")
+
+    } catch (e: FoodEmptyException) {
+        println("[ERROR] Stok Kurang: ${e.message}")
+
+    } catch (e: Exception) {
+        println("[ERROR] Terjadi kesalahan sistem: ${e.message}")
+    }
 }
