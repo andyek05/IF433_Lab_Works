6b595a40550ffcdba676dac3d9bac093d495feb0
Your Name <andy.ek788@gmail.com>
2026-06-14 22:04:49 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000115200_AndyEdlynsonKadiran/week13/CyptoTradeLogger.kt
@@ -0,0 +1,12 @@
+package oop_00000115200_AndyEdlynsonKadiran.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String, // "Long" atau "Short"
+    val margin: Double,
+    val pnl: Double
+)
